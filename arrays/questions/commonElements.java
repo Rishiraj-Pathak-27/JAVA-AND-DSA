6f191332a1861881b80b005f054d7bc4286a1f37
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-05-14 00:13:42 +0530

    common elements added in arrays

--- arrays/questions/commonElements.java
+++ arrays/questions/commonElements.java
@@ -91,3 +91,48 @@ public class commonElements{
     }
 } */
 
+// OR
+
+import java.util.Arrays;
+public class commonElements{
+    public static boolean helper(int[] arr, int target){
+        int left=0, right=arr.length-1;
+
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            
+            if(arr[mid] == target) return true;
+            else if(arr[mid] < target) left=mid+1;
+            else right=mid-1;
+        }
+        return false;
+    }
+
+    public static int[] commonFromBothArr(int[] arr1, int[] arr2){
+        int left=0, right=0;
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        for(int ele1:arr1){
+            if(helper(arr2,ele1)){
+                left++;
+            }
+        }
+
+        for(int ele2:arr2){
+            if(helper(arr1,ele2)){
+                right++;
+            }
+        }
+        return new int[] {left,right};
+    }
+
+    public static void main(String[] args){
+        int[] arr1 = {4,3,2,3,1};
+        int[] arr2 = {2,2,5,2,3,6};
+
+        int[] res = commonFromBothArr(arr1,arr2);
+        System.out.println(Arrays.toString(res));
+    }
+}
