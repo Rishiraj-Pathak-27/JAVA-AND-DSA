9e6e94b588f1ea3b17935e47cd17ef1e6fa5b40d
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-05-13 23:51:42 +0530

    Add implementation for finding common elements between two arrays with sorting

---
+++ Platform_Questions/leetcode/commonElements.java
@@ -0,0 +1,92 @@
+// 2956. Find Common Elements Between Two Arrays
+
+/* import java.util.Arrays;
+
+public class commonElements {
+
+    public static int[] commonFromBothArr(int[] arr1, int[] arr2){
+        int n=arr1.length;
+        int m=arr2.length;
+
+        int left=0, right=0;
+
+        for(int ele1:arr1){
+            for(int ele2:arr2){
+                if(ele1==ele2){
+                    left++;
+                    break;
+                }
+            }
+        }
+
+        for(int ele1:arr2){
+            for(int ele2:arr1){
+                if(ele1==ele2){
+                    right++;
+                    break;
+                }
+            }
+        }
+        return new int[] {left,right};
+    }
+
+    public static void main(String[] args){
+        int[] arr1 = {4,3,2,3,1};
+        int[] arr2 = {2,2,5,2,3,6};
+        
+        int[] res = commonFromBothArr(arr1,arr2);
+        System.out.println(Arrays.toString(res));
+    }
+} */
+
+
+// OR
+
+import java.util.Arrays;
+public class commonElements{
+
+    public static int[] commonFromBothArr(int[] arr1, int[] arr2){
+        int n=arr1.length;
+        int m=arr2.length;
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        int left=0, right=0;
+        int leftCount=0;
+
+        while(left<n && right<m){
+            if(arr1[left] == arr2[right]){
+                leftCount++;
+                left++;
+            }else if(arr1[left] > arr2[right]){
+                right++;
+            }else{
+                left++;
+            }
+        }
+
+        left=0;
+        right=0;
+        int rightCount=0;
+
+        while(left<n && right<m){
+            if(arr2[right] == arr1[left]){
+                rightCount++;
+                right++;
+            }else if(arr1[left] > arr2[right]){
+                right++;
+            }else{
+                left++;
+            }
+        }
+        return new int[]{leftCount,rightCount};
+    } 
+
+    public static void main(String[] args){
+        int[] arr1 = {4,3,2,3,1};
+        int[] arr2 = {2,2,5,2,3,6};
+
+        int[] res = commonFromBothArr(arr1,arr2);
+        System.out.println(Arrays.toString(res));
+    }
+} 
