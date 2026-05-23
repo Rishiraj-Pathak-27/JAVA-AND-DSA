9e253e376a209c82a3a7abb500c8a827e3eec3db
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-05-23 21:24:46 +0530

    Refactor powerOfNumbers class to implement power calculation using a simpler recursive approach and update main method to raise 5 to the power of 5.

--- Recursion/questions/powerOfNumbers.java
+++ Recursion/questions/powerOfNumbers.java
@@ -1,5 +1,40 @@
 // WAP to find out the power of number using recursion
 
+// TC O(log n)
+
+// public class powerOfNumbers {
+//     static int base;
+//     public static int reverse(int n){
+//         int reverse=0;
+        
+//         while(n!=0){
+//             int rem=n%10;
+//             reverse = (reverse*10)+rem;
+//             n/=10;
+//         }
+
+//         return reverse;
+//     }
+
+//     public static int power(int n){
+       
+//         if(n==0) return 1;
+//         int call = power(n/2);
+//         if(n%2==0) return call*call;
+//         else return base*call*call;
+        
+//     }
+//     public static void main(String[] args){
+//         int n=3;
+//         base=n;
+//         int reverse = reverse(n);
+//         System.out.println(power(reverse));
+//     }
+// }
+
+
+// OR (TC O(n))
+
 public class powerOfNumbers {
     static int base;
     public static int reverse(int n){
@@ -16,14 +51,12 @@ public class powerOfNumbers {
 
     public static int power(int n){
        
-        if(n==0) return 1;
-        int call = power(n/2);
-        if(n%2==0) return call*call;
-        else return base*call*call;
+       if(n==0) return 1;
+       return base*power(n-1);
         
     }
     public static void main(String[] args){
-        int n=3;
+        int n=5;  //5 to the power 5
         base=n;
         int reverse = reverse(n);
         System.out.println(power(reverse));
