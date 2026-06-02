87b61cc2691fae92498afb908e389fca002e4f2c
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-06-02 22:51:33 +0530

    Implement earliestFinishTimeForLandWater class to calculate earliest finish time for land and water tasks

---
+++ Platform_Questions/leetcode/earliestFinishTimeForLandWater.java
@@ -0,0 +1,29 @@
+public class earliestFinishTimeForLandWater{
+    public static void main(String[] args) {
+        int[] landStartTime = {2,8};
+        int[] landDuration = {4,1};
+        int[] waterStartTime = {6};
+        int[] waterDuration = {3};
+
+        System.out.println(earlistTime(landStartTime, landDuration, waterStartTime, waterDuration));
+    }
+
+    public static int earlistTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration){
+        int ans = Integer.MAX_VALUE;
+
+        for(int i=0; i<landStartTime.length; i++){
+            for(int j=0; j<waterStartTime.length; j++){
+                int landFinish = landStartTime[i]+landDuration[i];
+                int finishOne = Math.max(landFinish,waterStartTime[j]+waterDuration[j]);
+
+                int waterFinish = waterStartTime[j]+waterDuration[j];
+                int finishTwo = Math.max(waterFinish,landStartTime[i]+landDuration[i]);
+
+                ans = Math.min(finishOne,ans);
+                ans = Math.min(finishTwo,ans);
+               
+            }
+        }
+        return ans;
+    }
+}
