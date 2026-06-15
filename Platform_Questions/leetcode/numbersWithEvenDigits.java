3a792efe4756da7898542e0191f93c4a7b7ac049
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-06-15 21:32:30 +0530

    direct approach of problem ( 1295. Find Numbers with Even Number of Digits ) Leetcode

---
+++ Platform_Questions/leetcode/numbersWithEvenDigits.java
@@ -0,0 +1,30 @@
+// 1295. Find Numbers with Even Number of Digits
+
+// Direct Approach
+
+public class numbersWithEvenDigits{
+
+    public static int evenDigits(int[] arr){
+        int count=0;
+        int n=arr.length;
+
+        for(int i=0; i<n; i++){
+            int countDigit=0;
+            int num = arr[i];
+
+            while(num!=0){
+                int rem = num%10;
+                countDigit++;
+                num/=10;
+            }
+
+            if(countDigit%2==0) count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {234,1245,8982,90902837,1,22};
+        System.out.println(evenDigits(arr));
+    }
+}
