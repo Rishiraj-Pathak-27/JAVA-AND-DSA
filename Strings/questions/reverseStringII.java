584cc86be8d7050645a1ac155d02eb5ebe7d40af
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-06-22 21:36:52 +0530

    implement in-place reversal for Reverse String II

---
+++ Strings/questions/reverseStringII.java
@@ -0,0 +1,33 @@
+public class reverseStringII {
+
+    public static void reverse(char[] arr, int left, int right) {
+        while (left <= right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    public static String reverseStr(String s, int k) {
+
+        int reversePoint = 2*k;
+        char[] arr = s.toCharArray();
+        int n=s.length();
+        for(int i=0; i<s.length(); i+=reversePoint){
+
+            int left = i;
+            int right = Math.min(i+k-1,n-1);
+
+            reverse(arr,left,right);
+        }
+        return new String(arr);
+    }
+
+    public static void main(String[] args) {
+        String str = "acbdefghijk";
+        int k = 4;
+        System.out.println(reverseStr(str,k));
+    }
+}
