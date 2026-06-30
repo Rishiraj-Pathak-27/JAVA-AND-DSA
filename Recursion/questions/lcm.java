dc4e074354e60e34f87bc8d8168caefadb9991fd
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-06-30 23:29:51 +0530

    lcm code updated

--- Recursion/questions/lcm.java
+++ Recursion/questions/lcm.java
@@ -24,44 +24,44 @@
 
 // Normal Code
 
-public class lcm{
-    public static void main(String[] args) {
-        int a=12, b=18;
-        System.out.println(lcm(a,b));
-    }
-
-    public static int gcd(int a, int b){
-
-        int gcd=1;
-        for(int i=1; i<=Math.min(a,b); i++){
-            if(a%i == 0 && b%i == 0){
-                gcd=i;
-            }
-        }
-        return gcd;
-    }
-
-    public static int lcm(int a, int b){
-        return (a*b)/gcd(a,b);
-    }
-}
-
-// LCM Recursive Code
-
 // public class lcm{
 //     public static void main(String[] args) {
-//         int a=12, b=24;
-//         System.out.println(findLCM(a,b));
-
+//         int a=12, b=18;
+//         System.out.println(lcm(a,b));
 //     }
 
 //     public static int gcd(int a, int b){
-//         if(a==0) return b;
-//         return gcd(b%a,a);
+
+//         int gcd=1;
+//         for(int i=1; i<=Math.min(a,b); i++){
+//             if(a%i == 0 && b%i == 0){
+//                 gcd=i;
+//             }
+//         }
+//         return gcd;
 //     }
 
-//     public static int findLCM(int a, int b){
+//     public static int lcm(int a, int b){
 //         return (a*b)/gcd(a,b);
 //     }
 // }
+
+// LCM Recursive Code
+
+public class lcm{
+    public static void main(String[] args) {
+        int a=12, b=24;
+        System.out.println(findLCM(a,b));
+
+    }
+
+    public static int gcd(int a, int b){
+        if(a==0) return b;
+        return gcd(b%a,a);
+    }
+
+    public static int findLCM(int a, int b){
+        return (a*b)/gcd(a,b);
+    }
+}
 
