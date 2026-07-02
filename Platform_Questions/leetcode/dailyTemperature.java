6c61f2a9558ba2d14dd8b029c3b1381c5911d183
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-07-02 23:36:23 +0530

    another approach added for problem // 739. Daily Temperatures

--- Platform_Questions/leetcode/dailyTemperature.java
+++ Platform_Questions/leetcode/dailyTemperature.java
@@ -1,29 +1,61 @@
 // 739. Daily Temperatures
 
+// No Optimal Approach T.C(O(N^2))
+
+// import java.util.Arrays;
+
+// public class dailyTemperature {
+
+//     public static void main(String[] args) {
+//         int[] arr = {73, 74, 75, 71, 69, 72, 76, 73};
+
+//         System.out.println(Arrays.toString(dailyTemp(arr)));
+//     }
+
+//     public static int[] dailyTemp(int[] arr) {
+//         int n = arr.length;
+//         int[] ans = new int[n];
+
+//         for (int i = 0; i < n; i++) {
+//             int j = i + 1;
+//             while (j < n && arr[j] <= arr[i]) {
+//                 j++;
+//             }
+
+//             if (j < n) {
+//                 ans[i] = j - i;
+//             } else {
+//                 ans[i] = 0;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+// Optimal Approach T.C (O(N))
+
 import java.util.Arrays;
-
-public class dailyTemperature {
-
+public class dailyTemperature{
     public static void main(String[] args) {
-        int[] arr = {30,40,50,60};
-
+        int[] arr = {73,74,75,71,69,72,76,73};
         System.out.println(Arrays.toString(dailyTemp(arr)));
     }
 
-    public static int[] dailyTemp(int[] arr) {
-        int n = arr.length;
+    public static int[] dailyTemp(int[] arr){
+        int n=arr.length;
         int[] ans = new int[n];
 
-        for (int i = 0; i < n; i++) {
-            int j = i + 1;
-            while (j < n && arr[j] <= arr[i]) {
-                j++;
+        for(int i=n-2; i>=0; i--){
+            int j=i+1;
+            while(j<n && arr[j]<=arr[i]){
+                if(ans[j]==0){
+                    j=n;
+                }else{
+                    j+=ans[j];
+                }
             }
-
-            if (j < n) {
-                ans[i] = j - i;
-            } else {
-                ans[i] = 0;
+            if(j<n){
+                ans[i] = j-i;
             }
         }
         return ans;
