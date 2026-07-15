060c8548a13f8e9640f1861b3a40e736d0fb7a8f
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-07-15 22:41:44 +0530

    Add firstOccurrance class to implement binary search for the first occurrence of an element in a sorted array

---
+++ Recursion/OnArrays/firstOccurrance.java
@@ -0,0 +1,60 @@
+// Given a sorted array arr[] and an integer k, find the position(0-based indexing) at which k is present in the array using binary search.
+// If k doesn't exist in arr[] return -1. 
+
+// Approach Linear Search Recursively
+// public class firstOccurrance{
+//     public static void main(String[] args) {
+//         int[] arr = {1, 1, 1, 1, 2};
+//         int target=1;
+//         System.out.println(firstOcc(arr, target));
+//     }
+//     public static int helper(int[] arr, int target, int idx){
+//         int n=arr.length;
+//         if(idx==n) return -1;
+//         if(arr[idx]==target){
+//             return idx;
+//         }
+//         return helper(arr,target,idx+1);
+//     }
+//     public static int firstOcc(int[] arr, int target){
+//         int n=arr.length;
+//         return helper(arr,target,0);
+//     }
+// }
+// Binary Search Approach Recursively
+public class firstOccurrance {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 1, 1, 2};
+        int target = 1;
+
+        System.out.println(firstOcc(arr, target));
+    }
+
+    public static int helper(int[] arr, int target, int left, int right) {
+        int n = arr.length;
+        int ans = -1;
+        if (left > right) {
+            return ans;
+        }
+
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            ans = helper(arr, target, left, right - 1);
+            if (ans != -1) {
+                return ans;
+            }
+            return mid;
+        } else if (arr[mid] > target) {
+            return helper(arr, target, left, right - 1); 
+        }else {
+            return helper(arr, target, right + 1, right);
+        }
+    }
+
+    public static int firstOcc(int[] arr, int target) {
+        int n = arr.length;
+        return helper(arr, target, 0, n - 1);
+    }
+}
