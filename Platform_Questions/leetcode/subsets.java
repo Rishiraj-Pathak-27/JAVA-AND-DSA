e7c6eabf153d9e41ede78a2ef6f8e9a6d0b9fe13
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-07-16 23:47:29 +0530

    Add subsets class to implement the generation of all possible subsets of a unique integer array

--- Platform_Questions/leetcode/subsets.java
+++ Platform_Questions/leetcode/subsets.java
@@ -1,5 +1,4 @@
-// Given an integer array nums of unique elements, return all possible subsets (the power set).
-// The solution set must not contain duplicate subsets. Return the solution in any order.
+// 78. Subsets
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,27 +7,31 @@ public class subsets{
     public static void main(String[] args) {
         int[] arr = {1,2,3};
         System.out.println(subsetsPrint(arr));
-        
     }
-    
+
     public static List<List<Integer>> subsetsPrint(int[] arr){
 
-        List<List<Integer>> list = new ArrayList<>();
+        List<List<Integer>> list=new ArrayList<>();
         List<Integer> ans = new ArrayList<>();
+
         helper(ans,arr,0,list);
         return list;
-    }
-    public static void helper(List<Integer> ans, int[] arr, int idx, List<List<Integer>> list){
-        int n = arr.length;
 
-        if(n==idx) {
+    }
+
+    public static void helper(List<Integer> ans, int[] arr, int idx, List<List<Integer>> list){
+        int n=arr.length;
+
+        if(n==idx){
             list.add(new ArrayList<>(ans));
             return;
         }
 
         ans.add(arr[idx]);
+
         helper(ans,arr,idx+1,list);
         ans.remove(ans.size()-1);
         helper(ans,arr,idx+1,list);
+
     }
 }
