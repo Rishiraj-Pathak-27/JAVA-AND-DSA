b75e4927e54dc9dd713d15a1d44c3baf6e6d5385
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-07-18 20:17:36 +0530

    Add generateParentheses class to generate all combinations of well-formed parentheses

---
+++ Platform_Questions/leetcode/generateParentheses.java
@@ -0,0 +1,31 @@
+// 22. Generate Parentheses
+
+import java.util.ArrayList;
+import java.util.List;
+public class generateParentheses{
+    public static void main(String[] args) {
+        int n=4;
+        System.out.println(generate(n));
+    }
+
+    public static void helper(int n, int left, int right, String s,List<String> ans){
+        // base case
+        // if(s.length()==2*n){
+        //OR
+        if(right==n){
+            ans.add(s);
+            return;
+        }
+
+        // for left parentheses
+        if(left<n) helper(n,left+1,right,s+"(",ans);
+        // for right parentheses
+        if(right<left) helper(n,left,right+1,s+")",ans);
+    }
+
+    public static List<String> generate(int n) {
+        List<String> ans=new ArrayList<>();
+        helper(n,0,0,"",ans);   // n->given, 0->left parentheses, 0->right parentheses, ""->empty str to store well formed parentheses, ans->list to store grp of strs
+        return ans;
+    }
+}
