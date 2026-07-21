8cbd8cd416bb60552c21d26f3623d6689d8133c4
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-07-21 19:39:01 +0530

    Add distinctElementPerWindow class to count distinct elements in every sliding window

---
+++ Sliding-Window-Arr-Str/distinctElementPerWindow.java
@@ -0,0 +1,42 @@
+// Count Distinct Elements in Every Window
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class distinctElementPerWindow {
+    public static void main(String[] args){
+        int[] arr = {1,2,1,3,4,2,3};
+        int k=4;
+        System.out.println(distincts(arr,k));
+    }
+
+    public static List<Integer> distincts(int[] arr, int k){
+        int n=arr.length;
+
+        List<Integer> ans = new ArrayList<>();
+
+        for(int i=0; i<=n-k; i++){
+            int count=0;
+            for(int j=i; j<i+k; j++){
+                
+                boolean isFound=false;
+
+                for(int l=i; l<j; l++){
+                    if(arr[j] == arr[l]){
+                        isFound=true;
+                        break;
+                    }
+                }
+
+                if(!isFound){
+                    count++;
+                }
+
+            }
+
+            ans.add(count);
+        }
+        return ans;
+    }
+}
