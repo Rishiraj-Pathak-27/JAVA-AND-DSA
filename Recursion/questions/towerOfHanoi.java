08acc35e78e57971a85c6086a62e15d03a0bc9fd
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-07-22 15:39:59 +0530

    Add recursive implementation for Tower of Hanoi problem

--- Recursion/questions/towerOfHanoi.java
+++ Recursion/questions/towerOfHanoi.java
@@ -16,3 +16,32 @@ public class towerOfHanoi {
         hanoi(n-1,b,a,c);   // 2. Move n-1 disks from b -> c via a
     }
 }
+
+
+// AND
+
+// public class towerOfHanoi {
+//     static int count;
+//     public static void main(String[] args){
+//         int n=3;
+//         System.out.println(HanoiTower(n,'a','b','c'));
+//     }
+
+//     public static int HanoiTower(int n, char a, char b, char c){
+//         count=0;
+//         hanoi(n,a,b,c);
+//         return count;
+//     }
+
+//     public static int hanoi(int n, char a, char b, char c){       // a -> src, b -> via, c -> dest
+//         if(n==0) return 0;
+
+//         hanoi(n-1,a,c,b);   // 1. Move n-1 disks from a -> b via c 
+        
+//         count++;     // 2. move largest directly from a -> c
+
+//         hanoi(n-1,b,a,c);   // 2. Move n-1 disks from b -> c via a
+
+//         return count;
+//     }
+// }
