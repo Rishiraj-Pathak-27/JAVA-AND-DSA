0fd75dad97712d379aedf43e46688d094053998b
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-07-26 12:48:29 +0530

    Add basics class with main method for HashSet and TreeSet examples

---
+++ HashSet/basics.java
@@ -0,0 +1,19 @@
+public class basics{
+    public static void main(String[] args) {
+        // HashSet<Integer> set = new HashSet<>();
+        // set.add(9); set.add(24); set.add(43); set.add(11); set.add(9);
+        // System.out.println(set);
+        // System.out.println(set.size());
+        // System.out.println(set.contains(11)+" "+set.contains(100));
+        // set.remove(11);
+        // System.out.println(set.size());
+
+        // int[] arr = {5,4,3,2,1};
+        // TreeSet<Integer> set = new TreeSet<>();
+
+        // for(int ele:arr){
+        //     set.add(ele);
+        // }
+        // System.out.println(set);
+    }
+}
