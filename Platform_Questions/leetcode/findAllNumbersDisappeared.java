a7d082d2cfe44860a52affef7b60b3ec78c0376f
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-08-05 22:48:44 +0530

    Add findAllNumbersDisappeared class to identify missing numbers in an array

---
+++ Platform_Questions/leetcode/findAllNumbersDisappeared.java
@@ -0,0 +1,36 @@
+// 448. Find All Numbers Disappeared in an Array
+
+// arr size --> [1 -> n]
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class findAllNumbersDisappeared{
+    public static void main(String[] args){
+        int[] arr = {1,1};
+        System.out.println(disappearedElements(arr));
+    }
+
+    public static List<Integer> disappearedElements(int[] arr){
+        int n=arr.length;
+        List<Integer> ans = new ArrayList<>();
+
+        if(n==0) return ans;
+
+        HashSet<Integer> set = new HashSet<>();
+
+     
+        for(int ele:arr){
+            set.add(ele);
+            
+        }
+
+        for(int i=1; i<=n; i++){
+            if(!set.contains(i)){
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+}
