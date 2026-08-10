58a3af56ae797dae8045d05eefd49a75e5758577
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-08-10 20:52:21 +0530

    Add implementation of containsDuplicatesII class to check for duplicates within a given range

---
+++ HashMap/Questions/containsDuplicatesII.java
@@ -0,0 +1,22 @@
+// Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+import java.util.HashMap;
+
+public class containsDuplicatesII {
+    public static void main(String[] args){
+        int[] arr = {1,2,3,1};
+        int k=3;
+        System.out.println(duplicates(arr,k));
+    }
+
+    public static boolean duplicates(int[] arr, int k){
+        int n=arr.length;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        
+        for(int i=0; i<n; i++){
+            if(map.containsKey(arr[i]) && i-arr[i] <= k) return true;
+            map.put(arr[i],i);
+        }
+        return false;
+    }
+}
