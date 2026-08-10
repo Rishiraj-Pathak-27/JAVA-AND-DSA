99335a74b5f4e55870c31522a16108b39c189acb
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-08-10 20:47:13 +0530

    Add containsDuplicates class with problem statement for duplicate check within a range

--- Platform_Questions/leetcode/containsDuplicatesII.java
+++ Platform_Questions/leetcode/containsDuplicatesII.java
@@ -1,4 +1,4 @@
-// Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+// 219. Contains Duplicate II
 
 import java.util.HashMap;
 
