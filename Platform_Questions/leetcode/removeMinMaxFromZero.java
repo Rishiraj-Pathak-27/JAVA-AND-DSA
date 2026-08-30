8d089c79eff051615594b84a5741cc32d25be757
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-08-30 23:52:19 +0530

    // 2091. Removing Minimum and Maximum From Array  potd

---
+++ Platform_Questions/leetcode/removeMinMaxFromZero.java
@@ -0,0 +1,33 @@
+// 2091. Removing Minimum and Maximum From Array
+
+// You are given a 0-indexed array of distinct integers nums.
+// There is an element in nums that has the lowest value and an element that has the highest value. We call them the minimum and maximum respectively. Your goal is to remove both these elements from the array.
+// A deletion is defined as either removing an element from the front of the array or removing an element from the back of the array.
+// Return the minimum number of deletions it would take to remove both the minimum and maximum element from the array.
+
+public class removeMinMaxFromZero {
+    public static void main(String[] args){
+        int[] arr = {2,10,7,5,4,1,8,6};
+        System.out.println(removeMinMax(arr));
+    }
+
+    public static int removeMinMax(int[] arr){
+        int n=arr.length;
+        int minIdx=0, maxIdx=0;
+
+        for(int i=0; i<n; i++){
+            if(arr[i] > arr[maxIdx]) maxIdx=i; // maxIdx=
+            if(arr[i] < arr[minIdx]) minIdx=i;
+        }
+
+        int left=Math.min(maxIdx,minIdx);
+        int right=Math.max(maxIdx,minIdx);
+
+        int fr = right+1;
+        int bck = n-left;
+
+        int frBck = (left+1) + (n-right);
+        
+        return Math.min(fr,Math.min(bck,frBck));
+    }
+}
