a83e201ff3e51f9983e308ebd58857546061e4f9
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-09-02 23:54:20 +0530

    Add constructUniformParity class with main method and uniformArray function

---
+++ Platform_Questions/leetcode/constructUniformParity.java
@@ -0,0 +1,12 @@
+// 3875. Construct Uniform Parity Array I
+
+public class constructUniformParity {
+    public static void main(String[] args){
+        int[] arr = {2,3};
+        System.out.println(uniformArray(arr));
+    }
+
+    public static boolean uniformArray(int[] arr){
+        return true;
+    }
+}
