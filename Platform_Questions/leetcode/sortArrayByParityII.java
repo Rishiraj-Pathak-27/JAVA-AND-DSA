306170f451d27267889daa027aef9a1764120816
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-09-16 21:15:05 +0530

    Refactor sortArrayByParityII to inline helper method and improve sorting logic

--- Platform_Questions/leetcode/sortArrayByParityII.java
+++ Platform_Questions/leetcode/sortArrayByParityII.java
@@ -4,6 +4,41 @@
 // Sort the array so that whenever nums[i] is odd, i is odd, and whenever nums[i] is even, i is even.
 // Return any answer array that satisfies this condition.
 
+// import java.util.Arrays;
+
+// public class sortArrayByParityII {
+//     public static void main(String[] args){
+//         int[] arr = {4,2,5,7};
+//         System.out.println(Arrays.toString(sort(arr)));
+//     }
+
+//     public static void helper(int[] arr, int i, int j){
+//         int temp=arr[i];
+//         arr[i]=arr[j];
+//         arr[j]=temp;
+//     }
+
+//     public static int[] sort(int[] arr){
+//         int n=arr.length;
+
+//         int i=0, j=1;
+
+//         while(i<n && j<n){
+//             if(arr[i] % 2 == 0){
+//                 i+=2;
+//             }else if(arr[j] % 2 == 1){
+//                 j+=2;
+//             }else{
+//                 helper(arr,i,j);
+//             }
+//         }
+//         return arr;
+//     }
+// }
+
+
+// OR 
+
 import java.util.Arrays;
 
 public class sortArrayByParityII {
@@ -12,11 +47,6 @@ public class sortArrayByParityII {
         System.out.println(Arrays.toString(sort(arr)));
     }
 
-    public static void helper(int[] arr, int i, int j){
-        int temp=arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
-    }
 
     public static int[] sort(int[] arr){
         int n=arr.length;
@@ -29,7 +59,11 @@ public class sortArrayByParityII {
             }else if(arr[j] % 2 == 1){
                 j+=2;
             }else{
-                helper(arr,i,j);
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i+=2;
+                j+=2;
             }
         }
         return arr;
